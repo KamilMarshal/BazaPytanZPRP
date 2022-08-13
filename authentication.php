96aac136147ef1d55b0060c7f6bd49f965ed80c4
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2022-08-13 18:24:48 +0200

    Logowanie działa

--- authentication.php
+++ authentication.php
@@ -1,27 +1,41 @@
-<script src = "scripts.js"></script>
-<?php
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <?php
+    include("connection.php");
+    echo(' <link rel="stylesheet" href="style.css">');
 
-include("connection.php");
-//include("scripts.js");
-$username = $_POST["login"];
-$password = $_POST["password"];
+    $username = $_POST["login"];
+    $password = $_POST["password"];
 
-$sql = "SELECT * FROM login WHERE login = \"{$username}\" AND password = \"{$password}\"";
+    $sql = "SELECT * FROM login WHERE login = \"{$username}\" AND password = \"{$password}\"";
 
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "Witaj ". ucfirst($row["login"]). "!<br>";
-      $_SESSION["username"] = $row["login"];
-    }
-  } else {
-    echo "0 results";
-  }
-  $conn->close();
-  //echo ("<script> filechange(\"menu.php\"); </script>");
-  echo("<button onclick=\"location.href='menu.php'\">Click Me</button>");
-  
-
+    $result = $conn->query($sql);
 ?>
+</head>
+<body>
+  <div id="container">
+    <div id="loginWindow">
+      <span style="color:whitesmoke;">
+        <?php
+          if ($result->num_rows > 0) {
+              // output data of each row
+              while($row = $result->fetch_assoc()) 
+              {
+                echo("Poprawnie zalogowano! <br/>");
+                echo "Witaj ". $row["login"]. "!<br>";
+                $_SESSION["username"] = $row["login"];
+                header('Refresh: 1; URL=menu.php');
+              }
+            } else {
+              echo("Błędny login lub hasło! <br/>");
+              header('Refresh: 1; URL=index.php');
+            }
+            $conn->close();
+        ?>
+      </span>
+    </div>
+  </div>
+</body>
+</html>
