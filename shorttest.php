37bf6a15af21b4d877ce360444dfa2c75534c6a8
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2022-09-05 12:24:07 +0200

    css changes

--- shorttest.php
+++ shorttest.php
@@ -21,7 +21,8 @@
         <form>
             <?php 
                 include("connection.php");
-                $idQ = rand(0,399);
+                //$idQ = rand(0,399);
+                $idQ = 15;
                 $sql = "SELECT * FROM questions where id = ".$idQ;
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) 
@@ -30,24 +31,21 @@
                     $row = $result->fetch_array();
 
                     echo $row[1]."<br/>";
-                    echo $row[2]."<br/>";
+                    echo $row[2]."<br/><br/>";
 
-                    for($i=3;$i<(count($row))-3;$i++)
-                    {
-                        echo '<input type="checkbox" id ="odp'.$i.'"><br/>';
-                        echo '<label for="odp'.$i.'">'.$row[$i].'</label><br>';
-                    }
+                    for($i = 3; $i<12; $i+=1)
+                        if(isset($row[$i]))
+                        {
+                            echo '<input type="checkbox" id="odp'.$i.'" name="odp'.$i.'">';
+                            echo '<label for="odp'.$i.'">'.$row[$i].'</label><br>';
+                        }
                     
                 } 
 
                   $conn->close();
-
-
-
-            ?>            
-        </form>
-    </div>
-    
+            ?>
+        </form>                
+    </div>    
     
 
 
