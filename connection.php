3551df072c4a9c275185f7ec9298b00f6c9c1e9e
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2023-10-29 21:48:26 +0100

    change to online db login/passw

--- connection.php
+++ connection.php
@@ -1,13 +1,13 @@
 <?php      
     $host = "localhost";  
 
-    $user = "root";  
+    /*$user = "root";  
     $password = ''; 
-    $db_name = "marshalpl"; 
+    $db_name = "marshalpl"; */
 
-    #$user = "id21375490_marshalpl";  
-    #$password = '-TAH!xyd6Y6uV9ns';  
-    #$db_name = "id21375490_marshalpl";  
+    $user = "id21375490_marshalpl";  
+    $password = '-TAH!xyd6Y6uV9ns';  
+    $db_name = "id21375490_marshalpl";  
       
     $conn = new mysqli($host, $user, $password, $db_name);  
     if($conn->connect_error) {  
